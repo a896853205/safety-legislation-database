43d67fc8cfffc4470184cc49e44223498848a154
Qccccc's laptop <896853205@qq.com>
2020-04-25 21:58:27 +0800

    feat: 修改pm2进程名

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'API',
+      name: 'safetyAPI',
       script: './dist/www',
 
       // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
