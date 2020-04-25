7b971c9f1cea3d99a71e1d093b388198a33f73ea
Qccccc's laptop <896853205@qq.com>
2020-04-25 21:50:18 +0800

    chore: 自动部署工具

---
+++ ecosystem.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  apps: [
+    {
+      name: 'API',
+      script: './src/www',
+
+      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+      args: 'one two',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      },
+      env: {
+        COMMON_VARIABLE: 'true'
+      }
+    }
+  ],
+
+  deploy: {
+    production: {
+      user: 'root',
+      host: '47.94.133.12',
+      ref: 'origin/master',
+      repo: 'git@github.com:a896853205/safety-legislation-database.git',
+      path: '/safety-legislation/safety-legislation-server',
+      'post-deploy':
+        'npm install && tsc && pm2 reload ecosystem.config.js --env production',
+      'post-setup': 'npm install'
+    }
+  }
+};
