ab398342498c4e7a61c871637dfecc0952b30183
Qccccc's laptop <896853205@qq.com>
2020-04-29 17:25:15 +0800

    fix: 修改pro的解释器从node改为ts-node

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,24 @@
 module.exports = {
   apps: [
     {
+      interpreter: 'ts-node',
       name: 'safetyAPI',
       script: 'dist/www.js',
 
-      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-      args: 'one two',
       instances: 1,
       autorestart: true,
-      watch: false,
+      watch: true,
       max_memory_restart: '1G',
       env: {
-        NODE_ENV: 'development'
+        NODE_ENV: 'development',
       },
       env_production: {
-        NODE_ENV: 'production'
+        NODE_ENV: 'production',
       },
       env: {
-        COMMON_VARIABLE: 'true'
-      }
-    }
+        COMMON_VARIABLE: 'true',
+      },
+    },
   ],
 
   deploy: {
@@ -30,8 +29,8 @@ module.exports = {
       repo: 'git@github.com:a896853205/safety-legislation-database.git',
       path: '/safety-legislation/safety-legislation-server',
       'post-deploy':
-        'npm install && tsc && pm2 reload ecosystem.config.js --env production',
-      'post-setup': 'npm install && tsc'
-    }
-  }
+        'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-setup': 'npm install',
+    },
+  },
 };
